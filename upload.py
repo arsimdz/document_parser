fc3ec4c6f9c096970206030d865a0362e2e2fd27
arsimdz <darsim@student.ethz.ch>
2024-10-15 12:51:49 +0200

    linking to download page

--- upload.py
+++ upload.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, flash, redirect, url_for, send_file
+from flask import Flask, request, render_template, flash, redirect, url_for, send_file,send_from_directory
 import os
 from werkzeug.utils import secure_filename
 from doc_parse import doc_parser, documents_to_markdown
@@ -30,11 +30,22 @@ def upload_file():
         if os.listdir(app.config['UPLOAD_FOLDER'])!=[]:
             output = doc_parser(app.config['UPLOAD_FOLDER'])
             output_file = documents_to_markdown(output)
-            return send_file(output_file,as_attachment=True)
+            output_file = os.path.join('output',output_file)
+            file.save(output_file)
+            return redirect(url_for('download'))
             
 
         return redirect(url_for('upload_file'))
     return render_template('upload.html')
+
+@app.route('/download')
+def download():
+   return render_template('download.html', files=os.listdir('output'))
+
+@app.route('/download/<filename>')
+def download_file(filename):
+   return send_from_directory('output',path='output.md')
+
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
